95b890c0593135e8d0f16abbe7ecb130f0fa5759
Matiixx <87618425+Matiixx@users.noreply.github.com>
2022-12-28 11:02:43 +0100

    Init client site

---
+++ SRC/client/src/App.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const handleClick = async () => {
+    console.log("click");
+    const res = await axios.get('http://localhost:3000/')
+    console.log(await res.data);
+
+
+  }
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={handleClick}>
+          Click
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
