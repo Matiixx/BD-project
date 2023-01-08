63f221055acf372cfb4b1d8c7bcc30faa2c021c3
Matiixx <87618425+Matiixx@users.noreply.github.com>
2023-01-08 16:54:45 +0100

    Best users page

---
+++ SRC/client/pages/adminDashboard/bestUsers/index.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react'
+import { type NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import Header from "../../../components/Header";
+import useStore from "../../../store/useStore";
+import axios from 'axios';
+
+const URI = 'http://pascal.fis.agh.edu.pl:3040/0cichostepski/'
+
+interface IBestUser {
+  uzytkownik_id: number;
+  email: string;
+  ilosc_rezerwacji: number;
+}
+
+const BestUsers: NextPage = () => {
+  const router = useRouter();
+
+  const userId = useStore((state) => state.userId);
+  const userToken = useStore((state) => state.accessToken);
+
+  const [users, setUsers] = useState<IBestUser[]>([])
+
+  const userRole = useStore(state => state.userType)
+  useEffect(() => {
+    if (userRole === null) return;
+    if (userRole !== "admin") {
+      router.push("/userDashboard");
+      return;
+    }
+  }, [userRole])
+
+  useEffect(() => {
+    if (userId === undefined) {
+      router.push("/userLogin")
+      return;
+    }
+
+    if (userId !== null && userToken !== null && userRole === "admin") {
+      axios.get(URI + 'get/ranking-uzytkownikow', {
+        headers: {
+          Authorization: "Bearer " + userToken,
+        }
+      }).then(data => {
+        console.log(data);
+        setUsers(data.data)
+      }).catch(err => {
+        console.log(err);
+      })
+    }
+  }, [userId, userToken])
+
+  return (
+    <>
+      <Header />
+
+      <div>
+        <div className="flex flex-wrap justify-center mb-4">
+          <div
+            className="border-2 font-bold text-lg justify-around items-center flex-wrap rounded overflow-hidden m-2 flex flex-row gap-4 w-3/4 min-h-[50px] shadow-lg"
+          >
+            <div className="w-1/5">Email</div>
+            <div className="w-1/5">Ilosc rezerwacji</div>
+          </div>
+          {users ? (
+            users.map((el) => (
+              <div
+                className="border-2 text-lg justify-around items-center flex-wrap rounded overflow-hidden p-4 m-2 flex flex-row gap-4 w-3/4 min-h-[70px] shadow-lg"
+                key={el.uzytkownik_id}
+              >
+                <div className="w-1/5">{el.email}</div>
+                <div className="w-1/5">{el.ilosc_rezerwacji}</div>
+              </div>
+            ))
+          ) :
+            (null)
+          }
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BestUsers;
--- SRC/client/pages/adminDashboard/index.tsx
+++ SRC/client/pages/adminDashboard/index.tsx
@@ -57,6 +57,12 @@ const AdminDashboard: NextPage = () => {
           >
             Dodaj zakweterowanie
           </Link>
+          <Link
+            className="flex items-center justify-center w-1/4 h-32 text-xl bg-blue-500 hover:bg-blue-700 text-white font-bold  py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            href={"/adminDashboard/bestUsers"}
+          >
+            Przeglądaj najlepszych gości
+          </Link>
         </div>
       </div>
     </>
