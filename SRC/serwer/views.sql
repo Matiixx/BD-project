5b8b4fb27de78680f3d0e2ddd2f9412fec7d40ed
Matiixx <87618425+Matiixx@users.noreply.github.com>
2023-01-08 16:39:12 +0100

    Create views.sql

---
+++ SRC/serwer/views.sql
@@ -0,0 +1,26 @@
+-----------------------------------------------------------
+DROP VIEW IF EXISTS ranking_uzytkownikow;
+CREATE VIEW ranking_uzytkownikow AS
+SELECT uzytkownik_id,
+  email,
+  count(rezerwacja_id) as ilosc_rezerwacji
+from projekt."Uzytkownik"
+  JOIN projekt."Rezerwacja" USING(uzytkownik_id)
+WHERE typ_uzytkownika <> 'admin'
+GROUP BY uzytkownik_id
+HAVING count(rezerwacja_id) > 0
+ORDER BY ilosc_rezerwacji DESC
+LIMIT 10;
+-----------------------------------------------------------
+DROP VIEW IF EXISTS rezerwacje_bez_zakwaterowania;
+CREATE VIEW rezerwacje_bez_zakwaterowania AS
+SELECT zakwaterowanie_id,
+  rezerwacja_id,
+  data_rozpoczecia,
+  email,
+  czy_zakwaterowany
+from projekt."Rezerwacja"
+  JOIN projekt."Zakwaterowanie" USING(rezerwacja_id)
+  JOIN projekt."Uzytkownik" USING(uzytkownik_id)
+WHERE czy_zakwaterowany = false;
+-----------------------------------------------------------
