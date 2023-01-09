1d94ee303b4bed50084ffcc588e134acc065f7d5
Matiixx <87618425+Matiixx@users.noreply.github.com>
2023-01-09 17:03:33 +0100

    Update CORS

--- SRC/serwer/index.js
+++ SRC/serwer/index.js
@@ -11,7 +11,7 @@ const bodyParser = require('body-parser');
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({ origin: "*" }))
 app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
 
